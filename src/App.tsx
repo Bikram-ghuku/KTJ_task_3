8da44c6c8eab69d4d65ed17220d19bf36d3fe66e
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-25 18:55:14 +0530

    fix: update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './components/Home/Home';
-import Expenses from './components/Expenses/Expenses';
-import Error from './components/Error/Error';
+import Home from './components/Home';
+import Expenses from './components/Expenses';
+import Error from './components/Error';
 
 function App() {
 
