cfe58d43dc3e3c521ccf9150ea929599d81a9ca5
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-25 19:07:33 +0530

    feat: added navbar to main application

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Expenses from './components/Expenses';
 import Error from './components/Error';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/home' element={<Home/>} />
-        <Route path="/track" element={<Expenses/>} />
-        <Route path='/*' element={<Error/>} />
-      </Routes>
-    </Router>
+    <div>
+      <Navbar/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/home' element={<Home/>} />
+          <Route path="/track" element={<Expenses/>} />
+          <Route path='/*' element={<Error/>} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
