31d1f144c79c9306c8e34050be12cde2b73de96a
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-25 19:07:09 +0530

    feat: added navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './styles/Navbar.css';
 
-function Navbar() {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div></div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <a href="/">Expense Tracker</a>
+      </div>
+      <div className={`navbar-menu ${isOpen ? 'is-open' : ''}`}>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/track">Track</a>
+            </li>
+        </ul>
+      </div>
+      <div className="navbar-hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
